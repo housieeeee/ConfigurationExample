43466444fc9c1cc425c411f5c2249500c7c0f3e5
house <piyushkumarsometing@gmail.com>
2024-04-08 03:45:48 +0530

    ConfigurationAsAService

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using ConfigurationExample;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+//Supply an object of weatherApiOptions (with "weatehrapi" section) as a service
+builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherapi"));
 
 var app = builder.Build();
 
